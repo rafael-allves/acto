43ca4219fa64a281f2e0415c50797c3565616baf
rafael.dev.megleo <rafa.viana.moc@gmail.com>
2025-08-02 20:09:50 -0300

    main: cria arquitetura do projeto

---
+++ database/migrations/2025_08_02_223700_create_questions_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('questions', function (Blueprint $table) {
+            $table->id();
+
+            $table->foreignId('form_id')
+                ->constrained('forms')
+                ->cascadeOnDelete();
+
+            $table->string('text');
+            $table->enum('type', ['multiple_choice']);
+            $table->boolean('mandatory');
+            $table->float('peso')->default(1.0);
+
+            $table->softDeletes();
+            $table->timestamps();
+        });
+
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('questions');
+    }
+};
